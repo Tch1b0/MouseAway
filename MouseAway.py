548652e67fb18dbec938adb41072af2ef449690b
Tch1b0 <Jpour3006@gmail.com>
2020-06-26 17:40:08 +0200

    Added Classes

--- MouseAway.py
+++ MouseAway.py
@@ -8,33 +8,46 @@ clock = pygame.time.Clock()
 
 RED = (200,0,0)
 BLACK = (0,0,0)
-BLUE = (0,0,200)
+BLUE = (0,0,250)
 
 pos = [100,100]
 
-def Enemy():
-    pygame.draw.circle(screen, RED, pos, 20)
-    speed = 3
-    if pos[0] > mouse[0]:
-        pos[0] -= speed
-    if pos[0] < mouse[0]:
-        pos[0] += speed
-    if pos[1] > mouse[1]:
-        pos[1] -= speed
-    if pos[1] < mouse[1]:
-        pos[1] += speed
+class Player():
+    def __init__(self,mouse):
+        pygame.draw.circle(screen, BLUE, mouse, 10)
+        self.x = mouse[0]
+        self.y = mouse[1]
+        self.hitbox = (self.x + 1, self.y + 11, 29, 52)
+
+class Enemy():
+    def __init__(self,mouse):       
+        pygame.draw.circle(screen, RED, pos, 20)
+        speed = 2
+        if pos[0] > mouse[0]:
+            pos[0] -= speed
+        if pos[0] < mouse[0]:
+            pos[0] += speed
+        if pos[1] > mouse[1]:
+            pos[1] -= speed
+        if pos[1] < mouse[1]:
+            pos[1] += speed
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     screen.fill((BLACK))
     mouse = pygame.mouse.get_pos()
-    pygame.draw.circle(screen, BLUE, mouse, 10)
+    P = Player(mouse)
+    E = Enemy(mouse)
+    Player(mouse)
+    Enemy(mouse)
 
-    if pos[0] == mouse[0] and pos[1] == mouse[1]:
+    if pos == P.hitbox:
         quit()
 
-    Enemy()
+
 
     pygame.display.update()
     clock.tick(60)
